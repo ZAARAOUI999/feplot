b191bf92a80d8104433ae0e9a498a051a16e330b
Mohamed ZAARAOUI <115699524+ZAARAOUI999@users.noreply.github.com>
2022-11-12 14:15:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'feplot',
     packages = ['feplot'],
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Visualization tool for FElupe',
     author = 'Mohamed ZAARAOUI',
     author_email = 'mohamed.zaaraoui.999@gmail.com',
     url = 'https://github.com/ZAARAOUI999/feplot',
-    download_url = 'https://github.com/ZAARAOUI999/feplot/archive/refs/tags/0.0.2.tar.gz',
+    download_url = 'https://github.com/ZAARAOUI999/feplot/archive/refs/tags/0.0.3.tar.gz',
     keywords = ['felupe', 'visualization'],
     classifiers = [],
 )
