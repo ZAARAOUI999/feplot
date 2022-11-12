f818604495f9705cec37ea390ede3ca3df634368
Mohamed ZAARAOUI <115699524+ZAARAOUI999@users.noreply.github.com>
2022-11-12 14:22:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Mohamed ZAARAOUI',
     author_email = 'mohamed.zaaraoui.999@gmail.com',
     url = 'https://github.com/ZAARAOUI999/feplot',
-    download_url = 'https://github.com/ZAARAOUI999/feplot/archive/refs/tags/0.0.3.tar.gz',
+    download_url = 'https://github.com/ZAARAOUI999/feplot/archive/refs/tags/0.0.4.tar.gz',
     keywords = ['felupe', 'visualization'],
     classifiers = [],
 )
